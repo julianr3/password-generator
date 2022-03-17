89df0635610a585b6f18f6082bb24d216aaa4d0d
Julian Richardson <jrichar3@yahoo.com>
2022-03-17 14:54:58 -0400

    added follow-up questions to specify pwd requirements

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,12 @@ var generateBtn = document.querySelector("#generate");
 var length = Number(prompt("How many characters would you like to use for your password?"));
 while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("How many characters would you like to use for your password? (Length MUST be between 8-128 characters)"));
 
+// this is a list of requirements to confirm
+var uppers = confirm("Would you like to include uppercase letters?");
+var lowers = confirm("Would you like to include lowercase letters?");
+var numbers = confirm("Would you like to include numbers?");
+var symbols = confirm("Would you like to include special characters?");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
