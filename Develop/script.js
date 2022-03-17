d492edd317c55669d0ab169f343d21a976f740b4
Julian Richardson <jrichar3@yahoo.com>
2022-03-17 14:49:11 -0400

    added first prompt for user to specify pwd length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// this launches a prompt for user-input password length requirement
+var length = Number(prompt("How many characters would you like to use for your password?"));
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("How many characters would you like to use for your password? (Length MUST be between 8-128 characters)"));
 
 // Write password to the #password input
 function writePassword() {
